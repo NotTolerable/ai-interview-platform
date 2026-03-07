98f261eed00973d6bc99f186f26cb84ce016b90b
Allen Abraham <allenabraham106@gmail.com>
2026-03-06 22:03:39 -0500

    initial setup done

---
+++ Behaviourly/Behaviourly/CameraManage.swift
@@ -0,0 +1,7 @@
+//
+//  CameraManage.swift
+//  Behaviourly
+//
+//  Created by Allen Abraham on 2026-03-06.
+//
+
---
+++ Behaviourly/Behaviourly/CameraView.swift
@@ -0,0 +1,8 @@
+//
+//  CameraView.swift
+//  Behaviourly
+//
+//  Created by Allen Abraham on 2026-03-06.
+//
+
+// What the User sees (live preveiw)
